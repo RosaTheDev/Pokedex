0908b9e864f14abc439e321e82b5b6925772a627
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-03-02 22:30:11 -0700

    finally able to get the images rendering of the pokemon

--- src/Components/SinglePokemonCard/SinglePokemonCard.js
+++ src/Components/SinglePokemonCard/SinglePokemonCard.js
@@ -1,44 +1,39 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import PokemonCardAPI from "../../APICalls/PokemonCardAPI";
-import './SinglePokeContainer.css';
+import './SinglePokemonCard.css';
 
 class SinglePokemonCard extends Component {
   constructor({pokemonURL}) {
     super()
     this.state = {
-      pokemonInfo: []
+      pokemonInfo: [],
+      pokemonImg: ''
     }
     this.pokeURL = pokemonURL;
   }
 
   componentDidMount() {
     PokemonCardAPI(this.pokeURL)
-      .then(data => this.setState({pokemonInfo: data}))
+      .then(data => {
+        this.setState({ pokemonInfo: data, pokemonImg: data.sprites.other.dream_world.front_default})
+        console.log('data', data.sprites.other.dream_world.front_default)
+      })
   }
 
 
     render() {
-      console.log(this.state.pokemonInfo)
+      // console.log(this.state.pokemonInfo)
       return(
         <div>
-        <h1>Name {this.state.pokemonInfo.name}</h1>
-        {/* <img src={this.state.pokemonInfo.sprites.other.dream_world.front_default}  alt='pokemon'/> */}
+        <h1>Name: {this.state.pokemonInfo.name}</h1>
+        <h1>Height: {this.state.pokemonInfo.height} ft</h1>
+        <h1>Weight: {this.state.pokemonInfo.weight} lbs</h1>
+        <img src={this.state.pokemonImg} alt='a wild pokemon'/>
         </div>
       )
     }
 }
 
-// const SinglePokeContainer = ({pokemonInfo}) => {
-//   console.log({pokemonInfo})
-//   return(
-//     <div className='singlePokemon'>
-//       <h1>hello world</h1>
-//       {/* <h1>Name: {pokemonInfo.name}</h1>
-//        <img src={pokemonInfo.sprites.other.dream_world.front_default} alt="a pokemon"/>
-//       <p>Type:{pokemonInfo.types[0].type.name}</p> */}
-//      </div>
-//   )
-// }
 
 export default SinglePokemonCard;
